73cdc3ef2a6736004f8f06bf4262fa104dc1b6e2
fsande <fsande@ull.es>
2023-03-07 09:29:51 +0000

    Translation to TS

--- www/computePI/computePI.ts
+++ www/computePI/computePI.ts
@@ -1,31 +1,33 @@
 /**
- * @author F. de Sande
+ * @author       F. de Sande
  * @since 27.01.2020
  * @since 18.02.2021
- * @see {@link https://towardsdatascience.com/how-to-make-pi-part-1-d0b41a03111f}
+ * @since Mar 07, 2023 - TS version
  * 
  * Compute PI 
  * @description The code uses the method of Riemann Sums to approximate the area under a circle. 
  * This approximation is equal to (pi / 4). For more on Riemann Sums, see here.
  * Uses prompt and alert for input/output
+ *
+ * @see {@link https://towardsdatascience.com/how-to-make-pi-part-1-d0b41a03111f}
  */
 
-function CalculatePI() {
+function calculatePI(): void {
   let numRectangles = prompt("Enter number of rectangles:", "0");
-  if (numRectangles > 0) { 
-    let deltaX = 1.0 / numRectangles;
-    let x = 0;
-    let Pi = 0;
+  if (numRectangles && Number(numRectangles) > 0) {
+    let deltaX: number = 1.0 / Number(numRectangles);
+    let x: number = 0;
+    let Pi: number = 0;
     while (x < 1) {
-      let fx = Math.sqrt(1 - x * x);  // Circle with radius 1 and center at (0, 0)
+      let fx: number = Math.sqrt(1 - x * x);  // Circle with radius 1 and center at (0, 0)
       Pi += fx * deltaX;  // rectangle area
       x += deltaX;
-		  debugger;
+      debugger;
     }
     Pi = 4 * Pi;
     alert(Pi);
   }
   else {
     alert("Canceled or Error in input: Input must be positive.");
-	}
+  }
 }
